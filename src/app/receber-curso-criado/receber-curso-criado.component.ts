9aed7cd2c68c49bab87bb5b247c4871b63d6f296
José Luciano Julietti Rodrigues de Castro <joselucianorc@gmail.com>
2021-04-03 19:38:30 -0300

    Initial commit

---
+++ src/app/receber-curso-criado/receber-curso-criado.component.ts
@@ -0,0 +1,21 @@
+import { CursosService } from './../cursos/curso.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receber-curso-criado',
+  templateUrl: './receber-curso-criado.component.html',
+  styleUrls: ['./receber-curso-criado.component.css']
+})
+export class ReceberCursoCriadoComponent implements OnInit {
+
+  curso: string = '';
+
+  constructor(private cursosService: CursosService) {  }
+
+  ngOnInit(): void {
+    this.cursosService.emitirCursoCriado.subscribe(
+      cursoCriado => this.curso = cursoCriado
+    );
+  }
+
+}
